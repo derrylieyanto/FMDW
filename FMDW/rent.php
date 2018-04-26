2f3ad9f72bac07bb016948ae042d4a7adc2291da
Owen <owen.aloysius@ti.ukdw.ac.id>
2018-04-27 00:25:11 +0700

    add rent

--- FMDW/rent.php
+++ FMDW/rent.php
@@ -1,9 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>rent</title>
 </head>
 <body>
+	<header>
+		
+	</header>
+<!--         ==============================       -->	
+	<content>
+		 >> <a href="#">Rent</a>
+		 <p>RENT</p>
+		<form action="rent.html">
+		<input type="submit" value="RENT">
+		</form>
+		<form action="extention.html">
+		<input type="submit" value="EXTENTION">
+		</form>
+		<form action="return.html">
+		<input type="submit" value="RETURN">
+		</form>
+	</content>
+<!--         ==============================       -->	
+	<footer>
+		
+	</footer>
 
 </body>
 </html>
