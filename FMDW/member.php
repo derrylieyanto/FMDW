3f88d8372cbf766f3f844ac9258f2d8f7a7b5c5d
bobandresons <38763226+bobandresons@users.noreply.github.com>
2018-04-27 01:26:39 +0700

    Memberr
    ihiy

--- FMDW/member.php
+++ FMDW/member.php
@@ -4,6 +4,78 @@
 	<title></title>
 </head>
 <body>
+<div id="main-container">
 
+	<header>
+
+		<!--Navigasi bar kiei -->
+		<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Film</a></li>
+				<li><a href="#">Member</a></li>
+				<li><a href="#">Rent</a></li>
+				<li><a href="#">Account</a></li>
+				<li><a href="#">Report</a></li>
+			</ul>
+		</nav>
+		<!-- LOGO FMDW-->
+		<img src="images/FMDW.png" alt="Logo FMDW" id="logo" style="width: 50px; height: 50px;" />
+
+		<!-- Navigasi bar kanan -->
+		<!-- Search Box -->
+		<div class="search-container">
+    		<form action="/action_page.php">
+      			<input type="text" placeholder="Search.." name="search">
+      			<button type="submit" id="search-submit"><i class="fa fa-search"></i></button>
+      			<label>Hello. <?php ?></label>
+      			<a class="logout" href="#">LOG OUT</a>
+    		</form>
+  		</div>
+
+
+
+
+	</header>
+
+	<content>
+		
+		<div id="member-search">
+			<ul>
+				<li><a href="#">Member List</a></li>
+				<li><a href="#"><form action="/action_page.php">
+      			<input type="text" placeholder="Search.." name="search">
+      			<button type="submit" id="search-submit"><i class="fa fa-search"></i></button>
+      			<button type="submit" id="add"><i class="fa fa-search"></i></button></form></a></li>
+			</ul>
+		</div>
+		<div id="members">
+			<table>
+				<tr>
+					<td>asd</td>
+					<td colspan="2">dsa</td>
+				</tr>
+				<tr>
+					<td>ewq</td>
+					<td>qwe</td>
+				</tr>
+			</table>
+			<div>
+				<ul>
+					<li><a href="#">Shigeo Tokuda</a></li>
+					<li><a href="#"><img src="mahart.png"></a></li>
+				</ul>
+				<ul>
+					<li><a href="#">@shigeo69</a></li>
+				</ul>
+			</div>
+		</div>
+	</content>
+
+	<div id="footer">
+		<hr>
+		<p>Copyright 2018 FM DW. All Rights Reserved</p>
+	</div>
+</div>
 </body>
 </html>
