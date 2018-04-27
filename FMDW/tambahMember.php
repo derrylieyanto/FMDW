c4feba738a8dc97907c876c3b00ba42a8b445a3b
bobandresons <38763226+bobandresons@users.noreply.github.com>
2018-04-27 19:54:35 +0700

    Tambah Member + Member(nambahin)
    yg member ada kurang tag tadi

---
+++ FMDW/tambahMember.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TAMBAH MEMBER</title>
+	<link rel="stylesheet" type="text/css" href="style/header.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div id="main-container">
+
+	<header>
+			
+		<div class="logo-container">	
+		<img src="images/FMDW.png" alt="logo FMDW" class="logo">
+		</div>
+		<div class="topnav">
+	 			<a href="#">Home</a>
+				<a href="#">Film</a>
+				<a class="active" href="#">Member</a>
+				<a href="#">Rent</a>
+				<a href="#">Account</a>
+				<a href="#">Report</a>
+	  		
+  			<div class="search-container">
+    			<form action="/action_page.php">
+      				<input type="text" placeholder="Search.." name="search">
+      				<button type="submit"><i class="fa fa-search"></i></button>
+      				<label>Hello. <?php ?></label>
+      				<a class="logout" href="#">LOG OUT</a>
+    			</form>
+  			</div>
+  		</div>
+	</header>
+	
+	
+	<footer>
+		<div id="footer">
+		<hr>
+		<p>Copyright 2018 FM DW. All Rights Reserved</p>
+	</div>
+	</footer>
+</div>
+</body>
+</html>
