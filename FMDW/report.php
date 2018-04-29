1a4526e95a034b0d35d1e8e13bcbec7fb03a7072
derrylieyanto <32271541+derrylieyanto@users.noreply.github.com>
2018-04-30 02:11:19 +0700

    add script to table tab

--- FMDW/report.php
+++ FMDW/report.php
@@ -6,6 +6,17 @@
 	<link rel="stylesheet" type="text/css" href="style/footer.css">
 	<link rel="stylesheet" type="text/css" href="style/breadcrumb.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript">
+		function Historis(){
+			$("#historis").hide().fadeIn(500);
+			$("#status").hide();
+		}
+		function Status(){
+			$("#status").hide();
+			$("#historis").hide().fadeIn(500);
+		}
+	</script>
 	<style type="text/css">
 		#wrapper{
 			margin: auto;
