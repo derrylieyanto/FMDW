5451fbcd13cc6161b2b1b93671384cd3aad087e5
bobandresons <38763226+bobandresons@users.noreply.github.com>
2018-05-20 22:30:05 +0700

    Account + Account Dynamic

--- FMDW/account_detail.php
+++ FMDW/account_detail.php
@@ -1,3 +1,9 @@
+<?php
+  session_start();
+  include 'connect.php';
+  $id_account = $_GET['id_account'];
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,18 +24,18 @@
 	.menjorok input{
 		border-style: none;
 	}
-		 input[type=submit]{
-    background-color: #4CAF50;
-    border: none;
-    color: white;
-    padding: 16px 32px;
-    text-decoration: none;
-    margin: 4px 2px;
-    cursor: pointer;
+	input[type=submit]{
+	    background-color: #4CAF50;
+	    border: none;
+	    color: white;
+	    padding: 16px 32px;
+	    text-decoration: none;
+	    margin: 4px 2px;
+	    cursor: pointer;
     }
 
     input[type=submit]:hover{
-    background-color: #45a049;
+    	background-color: #45a049;
     }
 
     input[type=text]{
@@ -39,8 +45,8 @@
       font-size: 20px;
     }
     label {
-    padding: 12px 12px 12px 0;
-    display: inline-block;
+	    padding: 12px 12px 12px 0;
+	    display: inline-block;
 	 }
 
      input[type=text]:focus{
@@ -62,8 +68,7 @@
 		<img src="images/FMDW.png" alt="logo FMDW" class="logo">
 		</div>
 		<div class="topnav">
-	 			<a href="home.php" style="color: gray; font-size: 30px;padding: 6px 16px;
-"><i class="fa fa-home"></i></a>
+	 			<a href="home.php" style="color: gray; font-size: 30px;padding: 6px 16px;"><i class="fa fa-home"></i></a>
 				<a href="film.php">Film</a>
 				<a  href="member.php">Member</a>
 				<a href="rent.php">Rent</a>
@@ -85,22 +90,63 @@
 		<ul class="breadcrumb">
 		  <li><a href="home.php">Home</a></li>
 		  <li><a href="account.php">Account</a></li>
-		  <li>ajc98</li>
+		  <li>
+		      <?php 
+		        include 'connect.php';
+	    	    $sql = "SELECT * FROM account  WHERE id_account='$id_account'";
+	        	$result = mysqli_query($conn, $sql);
+	          
+		       	while($row = mysqli_fetch_array($result)) 
+		       	echo $row['nama']; 
+		     ?>
+		   </li>
 		</ul>
 		<div class="menjorok">
 			<h1>Detail Account</h1>
-			<h3>Username : ajc98</h3>
+			<h3>Username : <?php 
+		        include 'connect.php';
+	    	    $sql = "SELECT * FROM account  WHERE id_account='$id_account'";
+	        	$result = mysqli_query($conn, $sql);
+	          
+		       	while($row = mysqli_fetch_array($result)) 
+		       	echo $row['username']; 
+		     ?></h3>
 			<h2>Username <i class="fas fa-edit"></i></h2>
-			<input type="text" name="account_username" value="ajc98">
-			<h2>Privilege <i class="fas fa-edit"></i></h2>
-			<input type="text" name="account_privilege" value="MEMBER">
-			<h2>Last Online <i class="fas fa-edit"></i></h2>
-			<input type="date" name="account_last_online" value="2018-04-29">
-			<h2>Status <i class="fas fa-edit"></i></h2>
-			<form>
-				<input type="radio" name="account_status" value="Active">Active <br/>
-				<input type="radio" name="account_status" value="Banned" checked="checked">Banned
-			</form>
+			<?php 
+		        include 'connect.php';
+	    	    $sql = "SELECT * FROM account  WHERE id_account='$id_account'";
+	        	$result = mysqli_query($conn, $sql);
+	          
+		       	while($row = mysqli_fetch_array($result)) 
+		       	echo $row['username']; 
+		     ?>
+			<h2>Email <i class="fas fa-edit"></i></h2>
+			<?php 
+		        include 'connect.php';
+	    	    $sql = "SELECT * FROM account  WHERE id_account='$id_account'";
+	        	$result = mysqli_query($conn, $sql);
+	          
+		       	while($row = mysqli_fetch_array($result)) 
+		       	echo $row['email']; 
+		     ?>
+			<h2>Password <i class="fas fa-edit"></i></h2>
+			<?php 
+		        include 'connect.php';
+	    	    $sql = "SELECT * FROM account  WHERE id_account='$id_account'";
+	        	$result = mysqli_query($conn, $sql);
+	          
+		       	while($row = mysqli_fetch_array($result)) 
+		       	echo $row['password']; 
+		     ?>
+			<h2>Jabatan <i class="fas fa-edit"></i></h2>
+			<?php 
+		        include 'connect.php';
+	    	    $sql = "SELECT * FROM account  WHERE id_account='$id_account'";
+	        	$result = mysqli_query($conn, $sql);
+	          
+		       	while($row = mysqli_fetch_array($result)) 
+		       	echo $row['jabatan']; 
+		     ?>
 			<p></p>
 			<input type="submit" name="submit" value="Save">
 		</div>
