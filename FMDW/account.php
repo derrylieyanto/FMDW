64ed9aacf9c139dc638266239e943b6a5950ff94
Owen <owen.aloysius@ti.ukdw.ac.id>
2018-05-20 10:26:06 +0700

    fixed fontawesome

--- FMDW/account.php
+++ FMDW/account.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="style/footer.css">
 	<link rel="stylesheet" type="text/css" href="style/breadcrumb.css">
 	<script src="js/tableSort.js"></script>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 	<style type="text/css">
 		#wrapper{
 			 margin: auto;
