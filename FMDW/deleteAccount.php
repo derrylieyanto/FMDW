8d12f423c519f34ef83b2d7c016267c719c6df4a
Adrian Julio Candaya <adrian.julio@ti.ukdw.ac.id>
2018-05-21 09:45:17 +0700

    delete Acc

--- FMDW/deleteAccount.php
+++ FMDW/deleteAccount.php
@@ -3,14 +3,8 @@
 	session_start();
 	include 'connect.php';
 
-			$id_account = $_SESSION['id_account'];
-			$sql = "DELETE FROM account WHERE account.id_account = '$id_account'";
 
-			if (mysqli_query($conn, $sql)) {
-			    echo "delete successfully";
-			    header("location:account.php");
-			} else {
-			    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-			}
+	mysqli_query($conn, "DELETE FROM account WHERE id_account='$id_account'")
+  or die(mysqli_error($conn));
 
 ?>
